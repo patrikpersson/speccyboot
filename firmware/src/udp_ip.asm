6d7d5c16fd96852ec5d4282d63922123a376b11c
Patrik Persson <patrik.j.persson@gmail.com>
2021-05-13 19:15:34 +0200

    moved UDP/IP to asm

--- firmware/src/udp_ip.asm
+++ firmware/src/udp_ip.asm
@@ -1,49 +1,54 @@
-/*
- * Module udp_ip:
- *
- * User Datagram Protocol (UDP, RFC 768)
- * Internet Protocol (IP, RFC 791)
- *
- * Part of SpeccyBoot <https://github.com/patrikpersson/speccyboot>
- *
- * ----------------------------------------------------------------------------
- *
- * Copyright (c) 2009-  Patrik Persson
- *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following
- * conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- */
+;; Module udp_ip:
+;;
+;; User Datagram Protocol (UDP, RFC 768)
+;; Internet Protocol (IP, RFC 791)
+;;
+;; Part of SpeccyBoot <https://github.com/patrikpersson/speccyboot>
+;;
+;; ----------------------------------------------------------------------------
+;;
+;; Copyright (c) 2009-  Patrik Persson
+;;
+;; Permission is hereby granted, free of charge, to any person
+;; obtaining a copy of this software and associated documentation
+;; files (the "Software"), to deal in the Software without
+;; restriction, including without limitation the rights to use,
+;; copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the
+;; Software is furnished to do so, subject to the following
+;; conditions:
+;;
+;; The above copyright notice and this permission notice shall be
+;; included in all copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+;; OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+;; HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+;; WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+;; FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+;; OTHER DEALINGS IN THE SOFTWARE.
 
-#include "udp_ip.h"
+    .module udp_ip
+    .optsdcc -mz80
 
-#include "eth.h"
-#include "globals.h"
+    .include "include/udp_ip.inc"
 
-/* ------------------------------------------------------------------------- */
+    .include "include/bootp.inc"
+    .include "include/enc28j60.inc"
+    .include "include/eth.inc"
+    .include "include/globals.inc"
+    .include "include/tftp.inc"
+    .include "include/util.inc"
 
-void
-ip_receive(void)
-__naked
-{
-  __asm
+    .area _CODE
+
+;; ############################################################################
+;; _ip_receive
+;; ############################################################################
+
+_ip_receive:
 
     ;; clear IP checksum
 
@@ -225,20 +230,11 @@ ip_receive_check_checksum::
     pop  af   ;; pop return address within ip_receive
     ret       ;; return to _caller_ of ip_receive
 
-  __endasm;
-}
+;; ############################################################################
+;; _udp_create_impl
+;; ############################################################################
 
-/* ------------------------------------------------------------------------- */
-
-void
-udp_create_impl(const struct mac_address_t  *dst_hwaddr,
-                const ipv4_address_t        *dst_ipaddr,
-                uint16_t                     udp_length)
-__naked
-{
-  (void) dst_hwaddr, dst_ipaddr, udp_length;
-
-  __asm
+_udp_create_impl:
 
     push  ix
 
@@ -348,10 +344,10 @@ __naked
     ld     hl, #_header_template
     jp     _enc28j60_write_memory_cont
 
-    ;; ----------------------------------------------------------------------
-    ;; IP header defaults
-    ;; https://en.wikipedia.org/wiki/IPv4#Header
-    ;; ----------------------------------------------------------------------
+;; ============================================================================
+;; IP header defaults
+;; https://en.wikipedia.org/wiki/IPv4#Header
+;; ============================================================================
 
 ip_header_defaults::
     .db   0x45, 0            ;; version, IHL, DSCP, EN
@@ -361,6 +357,3 @@ ip_header_defaults::
     .db   0x40               ;; time to live
     .db   IP_PROTOCOL_UDP    ;; protocol
     .dw   0                  ;; checksum (temporary value for computation)
-
-  __endasm;
-}
